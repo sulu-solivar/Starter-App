57ae8d222e34a7b7990a1dd0defdb0c83749c486
Eshwar Chandra <eshwarcc@gmail.com>
2012-06-10 23:39:40 +0530

    added config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Starter::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # added to by pass the precompilation error of css and js
+  config.assets.precompile += %w[active_admin.css active_admin.js]
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
